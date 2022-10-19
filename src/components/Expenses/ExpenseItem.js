93c4f444019185d8305c334b44e2386bedad9837
Florencia Dollanarte <florenciadollanarte@gmail.com>
2022-10-19 16:28:47 -0300

    Added Expense Form and New Expense comp

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,18 @@
+import React, { useState } from 'react';
+
 import "./ExpenseItem.css";
 import Card from '../UI/Card'; 
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {  
-  let title = props.title;
-
-  const clickHandler = (props) => {
-    console.log("Updated!")
+  //acá está usando array destructuring, el 1er elem de use state es el current value
+  //y el 2do valor que devuelve useState es una función para updatear el valor del 1er elem
+  const [title, setTitle] = useState(props.title);
+  console.log("ExpenseItem being evaluated by React N times per component created");
+  
+  const clickHandler = () => {
+    setTitle('Updated title!');
+    console.log(title);
   }
 
   return (
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -21,9 +21,9 @@ const Expenses = (props) => {
         date={props.items[2].date}
       />
       <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
       />
     </Card>
   );
